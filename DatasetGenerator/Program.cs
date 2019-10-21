b920d9790cfd6f0fd0d094657313faea811dfa42
a.barkin <a.barkin@danycom.ru>
2019-10-21 19:44:52 +0300

    dataset generator

---
+++ DatasetGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DatasetGenerator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Generator gen = new Generator();
+            gen.Generate();
+
+            Console.WriteLine("Done");
+        }
+    }
+}
