0ee510f1c9ea87d0903ca4247a2440c9f2fd5ee3
a.barkin <a.barkin@danycom.ru>
2020-02-03 09:07:30 +0300

    ML

--- PassportRecognizer/ConvolutionNet.cs
+++ PassportRecognizer/ConvolutionNet.cs
@@ -1,7 +1,13 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
+using System.Linq;
 using DatasetGenerator;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
 using PassportRecognizerML.Model;
+using Point = OpenCvSharp.Point;
 
 namespace PassportRecognizer
 {
@@ -10,11 +16,16 @@ namespace PassportRecognizer
     /// </summary>
     internal class ConvolutionNet
     {
+        private HandwritingRecognition handWriting = new HandwritingRecognition();
+        private int cnt = 0;
+
         public void RecognizeLetter(string filename)
         {
-            if (!File.Exists(filename)) return;
+            string file = @"C:\Temp\tiffs\mat27.png";
 
-            Bitmap bit = new Bitmap(filename);
+            if (!File.Exists(file)) return;
+
+            Bitmap bit = new Bitmap(file);
             Bitmap resized = new Bitmap(bit, 32, 32);
 
             var hand = new HandwritingRecognition();
@@ -24,10 +35,48 @@ namespace PassportRecognizer
             input.PixelValues = data.ToArray();
 
             ModelOutput result = ConsumeModel.Predict(input);
+
+            Console.WriteLine($"Prediction: {Convert.ToChar((int)result.Prediction + '0')} \tScore: {result.Score.Max()}");
         }
 
-        public void RecognizeImage(Bitmap image)
+        // Вообще не вариант. Очень много времени берет
+        public void GetPossibleLetters(Mat mat)
         {
+            int width = mat.Width;
+            int height = mat.Height;
+
+            List<Point> points = new List<Point>();
+
+            for (int h = 0; h < height - 32; h += 4)
+            {
+                for (int w = 0; w < width - 32; w += 4)
+                {
+                    Mat image = new Mat(mat, new Rect(w, h, 32, 32));
+
+                    var model = RecognizeImage(image.ToBitmap());
+                    if (model.Score.Max() > 20)
+                    {
+                        points.Add(new Point(w, h));
+                    }
+                }
+                Console.WriteLine($"ROW {h}\t points: {points.Count}");
+            }
+
+            Console.WriteLine($"Count: {points.Count}");
+        }
+
+        public ModelOutput RecognizeImage(Bitmap image)
+        {
+            image.Save($@"c:/Temp/{cnt++}.png");
+
+            var data = handWriting.GetDatasetValues(image, "ffffffff");
+
+            var input = new ModelInput
+            {
+                PixelValues = data.ToArray()
+            };
+
+            return ConsumeModel.Predict(input);
         }
     }
 }
